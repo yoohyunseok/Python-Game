4fdfac570cb63c1fbebb3a1f054a548f909b485d
yoohyunseok <127180313+yoohyunseok@users.noreply.github.com>
2026-01-31 23:41:24 +0900

    Add files via upload

--- python_game/chapter6/list0605_1.py
+++ python_game/chapter6/list0605_1.py
@@ -7,9 +7,4 @@ canvas = tkinter.Canvas(root, width=800, height=600) #캔버스 컴포넌트 생
 canvas.pack() #윈도우 캔버스 배치
 gazou = tkinter.PhotoImage(file = "miko.png") #legend_guitar에 이미지 파일 로딩
 canvas.create_image(400, 300, image = gazou) #캔버스에 이미지 그리기
-label = tkinter.Label(root, text="??", font=("Times New Roman", 120), bg = "white") #라벨 컴포넌트 생성
-label.place(x=380, y=60) #라벨 위치 설정
-button = tkinter.Button(root, text = "제비 뽑기", font = ("Times New Roman", 36), fg="skyblue") #버튼 컴포넌트 생성, 문자열 색상 fg로 설정
-button.place(x=360, y=400) # 윈도우에 버튼 배치
-
 root.mainloop()
